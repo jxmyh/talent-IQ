57c7bb9303b68f02039f762d3d4042354c0eb69e
jxmyh <jxmyh99@gmail.com>
2025-12-09 08:58:39 +0800

    移除vite-plugin-vercel插件

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
+  plugins: [react()],
 });
