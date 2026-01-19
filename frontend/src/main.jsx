67cc7bb80f91ca991fe98f6095b9d9d2d2cac77b
jxmyh <jxmyh99@gmail.com>
2026-01-19 22:29:41 +0800

    feat: 添加前端基础架构和路由功能
    添加 TailwindCSS、DaisyUI、React Router 和 React Query 依赖
    实现基本路由结构和页面组件
    配置 Vite 支持 TailwindCSS
    添加 axios 实例和 toast 通知功能

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
 import './index.css';
 import App from './App.jsx';
+import { BrowserRouter } from 'react-router';
+
+import { QueryClient } from '@tanstack/react-query';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -10,10 +13,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing clerk Publishable Key');
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <queryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+          <App />
+        </ClerkProvider>
+      </queryClientProvider>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignInButton,
+  UserButton,
+} from '@clerk/clerk-react';
+import toast from 'react-hot-toast';
+function HomePage() {
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={() => toast.success('This is a  success toast')}>
+        click me
+      </button>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button>login</button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+
+      <UserButton />
+    </div>
+  );
+}
+
+export default HomePage;
