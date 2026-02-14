f38d2cdfaf0de695b2c0a27af3e3fb297b5b4c17
jxmyh <jxmyh99@gmail.com>
2026-02-14 11:39:01 +0800

    feat: 添加主页和仪表盘页面并改进UI
    添加新的仪表盘页面并重构主页UI
    引入Lucide图标库
    添加主题支持和按钮光标样式
    优化认证状态加载逻辑

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,13 +10,23 @@ import { Navigate, Route, Routes } from 'react-router';
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
 import { Toaster } from 'react-hot-toast';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded } = useUser();
+  // this will get rid of the flickering effect 这将消除闪烁效果
+  if (!isLoaded) return null;
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/"
+          element={!isSignedIn ? <HomePage /> : <Navigate to={'/dashboard'} />}
+        />
+        <Route
+          path="/dashboard"
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={'/'} />}
+        />
         <Route
           path="/problems"
           element={isSignedIn ? <ProblemsPage /> : <Navigate to={'/'} />}
